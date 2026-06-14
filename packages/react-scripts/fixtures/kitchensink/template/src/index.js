4748a8dcc814c7c46d0b71921b6e200b54eb9a07
Taiseer22 <162822227+Taiseer22@users.noreply.github.com>
2026-06-14 14:55:18 +0300

    Revert "طيار"

--- packages/react-scripts/fixtures/kitchensink/template/src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * LICENSE file in the root directory of this source tree.
- */
-
-import ReactDOMClient from 'react-dom/client';
-import App from './App';
-
-ReactDOMClient.createRoot(document.getElementById('root')).render(<App />);
